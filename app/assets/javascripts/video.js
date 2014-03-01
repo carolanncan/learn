6ef958dc08a8e5eb69dbfc1a18fa5b9cc7fabcb2
Carolann Bonner <carolanncan@gmail.com>
2014-02-28 18:28:39 -0800

    Add video player styles

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -1,9 +1,10 @@
+var video = document.getElementsByTagName('video');
 var video = document.createElement('video');
 var playPause = document.getElementsByClassName("play-pause")[0];
 var videoIsPlaying = false;
 var slider = document.getElementById('volume');
 
-video.setAttribute("src", "http://avengethevirgins.net/wp-content/uploads/2013/08/Drake-Hold-On-Were-Going-Home.mp3")
+video.setAttribute("src", "http://www.w3schools.com/html/mov_bbb.mp4")
 
 playPause.addEventListener("click", function(e){
     if (!videoIsPlaying){
